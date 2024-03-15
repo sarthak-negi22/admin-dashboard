d91e9753346b6b57220008bed52f6cb2ee38ca6a
Sarthak <saryatnam22@gmail.com>
2024-03-15 23:20:48 +0530

    added custom error.tsx and not-found.tsx for error handling

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 
-
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -16,11 +15,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <p
       className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to Acme.</strong> <br/> This is your personalized {' '}
+            <a href="https://github.com/sarthak-negi22/admin-dashboard" target = "_blank" className="text-blue-500">
+              Next.js-powered {' '}
             </a>
-            , brought to you by Vercel.
+            Invoice Management Admin Dashboard
           </p>
           <Link
             href="/login"
